4f3fc68c1edfe203f8479576e375c17b6184022e
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-03-01 18:18:18 +0200

    sidebar heading icon

--- cryptx-project/src/components/layout/Sidebar.jsx
+++ cryptx-project/src/components/layout/Sidebar.jsx
@@ -1,53 +1,77 @@
 import React from 'react'
+import { useState } from "react";
 import { HiSquares2X2 } from "react-icons/hi2";
-import { LuChartPie, LuWalletCards, LuSettings } from "react-icons/lu";
-import { PiWallet } from "react-icons/pi";
+import { LuChartPie, LuWalletCards, LuSettings, LuWallet } from "react-icons/lu";
 import { FiMail } from "react-icons/fi";
 import { BiLogOut } from "react-icons/bi";
+import { IoInfinite } from "react-icons/io5";
 
 function Sidebar() {
+  const [active, setActive] = useState("Overview")
+
+  const menu = [
+    {
+      label: "Overview",
+      icon: HiSquares2X2,
+    },
+    {
+      label: "Chart",
+      icon: LuChartPie,
+    },
+    {
+      label: "Transactions",
+      icon: LuWalletCards,
+    },
+    {
+      label: "Wallet",
+      icon: LuWallet,
+    },
+    {
+      label: "Mail Box",
+      icon: FiMail,
+    },
+    {
+      label: "Settings",
+      icon: LuSettings,
+    },
+    {
+      label: "Logout",
+      icon: BiLogOut,
+    }
+  ]
   return (
-    <div className='bg-gray-100 p-6'>
-      <div>
-        <div className='text-2xl font-bold pb-12'>
+    <aside className='bg-gray-100 p-6 w-64'>
+
+        {/* LOGO */}
+        <div className='flex items-center gap-2 text-2xl font-bold pb-12'>
+          <IoInfinite className='text-blue-500 w-10 h-10'/>
           CryptX
         </div>
 
-      <aside className='w-45'>
         <ul className='space-y-6'>
-          <li className='flex gap-4 text-base'>
-            <HiSquares2X2 className='w-6 h-6' />
-            Overview
+          {menu.map((item) => {
+            const Icon = item.icon;
+            const isActive = active === item.label;
+          
+          return (
+          <li 
+            key={item.label}  
+            className={`relative flex items-center gap-4 text-base ${
+            isActive ? "text-blue-500" : "text-gray-400"}`}
+          >
+            <Icon className="text-xl" />
+            <span className='font-medium'>{item.label}</span>
+
+            {isActive && (
+              <span className='absolute right-3 w-2 h-2 bg-blue-500 rounded-full' />
+            )}
+
           </li>
-          <li className='flex gap-4 text-base'>
-            <LuChartPie className='w-6 h-6' />
-            Chart
-          </li>
-          <li className='flex gap-4 text-base'>
-            <PiWallet className='w-6 h-6' />
-            Transactions
-          </li>
-          <li className='flex gap-4 text-base'>
-            <LuWalletCards className='w-6 h-6' />
-            Wallet
-          </li>
-          <li className='flex gap-4 text-base'>
-            <FiMail className='w-6 h-6' />
-            Mail Box
-          </li>
-          <li className='flex gap-4 text-base'>
-            <LuSettings className='w-6 h-6' />
-            Setting
-          </li>
-          <li className='flex gap-4 text-base'>
-            <BiLogOut className='w-6 h-6' />
-            Logout
-          </li>
+          );
+          })}
         </ul>
       </aside>
         
-      </div>
-    </div>
   )
 }
 
